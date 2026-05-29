3ef22c633420d43cbcc75702b1748262f5d84502
Kak Wa <champgod011@gmail.com>
2026-05-29 18:30:19 +0700

    Update aim position calculation with prediction

--- main.lua
+++ main.lua
@@ -560,7 +560,7 @@ if SendRemote and SendRemote.FireServer then
                 local root = aimTarget.Character and aimTarget.Character:FindFirstChild("HumanoidRootPart")
                 local hum  = aimTarget.Character and aimTarget.Character:FindFirstChild("Humanoid")
                 if head and root and hum then
-                    local aimPos = head.Position
+                    local aimPos = predictPosition(head, root)
                     local myHead     = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Head")
                     local originPos  = myHead and myHead.Position or nil
 
